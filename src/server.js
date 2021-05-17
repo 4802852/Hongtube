540a0da879086f7a96c935e89283b4f759ab60f4
hongseok choi <4802852@gmail.com>
2021-05-17 21:16:42 +0900

    mongodb on working

--- src/server.js
+++ src/server.js
@@ -9,9 +9,14 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
+// view engine 으로 pug 사용
 app.set("view engine", "pug");
+// views 기본 폴더 변경
 app.set("views", process.cwd() + "/src/views");
+// morgan("dev") 사용. 웹사이트 이용 시 console 에 정보 표기
 app.use(logger);
+// post 된 정보를 처리하기 위한 middleware
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
