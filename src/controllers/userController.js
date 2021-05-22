1c1b54bb899b15f99247354df89d49718baedec0
hongseok choi <4802852@gmail.com>
2021-05-22 14:32:12 +0900

    user join on working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,32 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/user";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+    console.log(req.body);
+    const { name, email, username, password, password2, location } = req.body;
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        });
+    };
+    if (exists) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This username or email is already taken.",
+        });
+    };
+    await User.create({
+        name,
+        email,
+        username,
+        password,
+        location,
+    });
+    res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("login");
