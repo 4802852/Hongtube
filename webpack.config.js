a76204008a9920ec5f29fbfe27b77c530784ea22
hongseok choi <4802852@gmail.com>
2021-05-30 21:01:09 +0900

    recorder done

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
 const path = require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   },
   mode: "development",
   watch: true,
-  plugins: [new MiniCssExtractPlugin({
-    filename: "css/styles.css",
-  })],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         use: {
           loader: "babel-loader",
           options: {
             presets: [
-              ["@babel/preset-env", {
-                targets: "defaults"
-              }]
+              [
+                "@babel/preset-env",
+                {
+                  targets: "defaults",
+                },
+              ],
             ],
           },
         },
@@ -34,7 +41,7 @@ module.exports = {
         test: /\.scss$/,
         // use: ["style-loader", "css-loader", "sass-loader"],
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      }
+      },
     ],
   },
-}
+};
